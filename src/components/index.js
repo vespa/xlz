42ceb7b41f0c1bdb8a6e319bfbb352aa7f1c09bd
Danilo Vespa <danilo.vespa@everskill.de>
2022-01-16 17:29:42 +0100

    unused element removed

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ export * from "components/Container";
 export * from "components/Row";
 export * from "components/Col";
 export * from "components/Header";
-export * from "components/Input";
 export * from "components/SearchBox";
 export * from "components/Button";
