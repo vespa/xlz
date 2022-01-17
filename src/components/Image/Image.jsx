8c58c53d42ef14161736f75492a687499eb9fd3f
Danilo Vespa <danilo.vespa@everskill.de>
2022-01-17 15:09:15 +0100

    configure lazy load

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useCallback, useState } from 'react'
+import React, { useEffect, useCallback, useState, useRef } from 'react'
 import PropTypes from "prop-types";
 import styles from "./Image.module.scss"
 /**
  * Load lazy Image 
  */
 
+
+
+
 export const Image = ({ src, alt, className = "", ...rest }) => {
     const [currentSource, setCurrentSource] = useState("")
-
+    const imageEl = useRef(null)
     // it deals with the random redirecting provided on the fake api
     const loadImage = useCallback(
         async () => {
@@ -16,17 +19,55 @@ export const Image = ({ src, alt, className = "", ...rest }) => {
         }, [src]
     )
 
-    useEffect(() => {
-        loadImage()
+    const preload = (elem) => {
+        elem.setAttribute('src', elem.getAttribute('data-src'))
+    }
+
+    const checkVisible = (elm) => {
+        var { bottom, top } = elm.getBoundingClientRect();
+        var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+        return !(bottom < 0 || top - viewHeight >= 0);
+    }
+
+    const isImageVisible = useCallback(async () => {
+        if (checkVisible(imageEl.current)) {
+            window.removeEventListener('scroll', isImageVisible, false)
+            await loadImage()
+            preload(imageEl.current.querySelector('img'))
+        }
     }, [loadImage])
+
+    const configureLazyLoad = useCallback(() => {
+        const elem = imageEl.current
+        elem.style.height = window.getComputedStyle(elem).width
+        !checkVisible(imageEl.current) && window.addEventListener("scroll", isImageVisible, false)
+    }, [isImageVisible])
+
+
+
+    useEffect(() => {
+        isImageVisible()
+    }, [isImageVisible])
+
+    useEffect(() => {
+        imageEl.current && configureLazyLoad()
+    }, [configureLazyLoad])
 
     return (
         <>
-            {!!currentSource && <img src={currentSource} alt={alt} {...rest} className={`${styles.image} `} />}
+            {<div ref={imageEl} className={`${styles.image}`} >
+                {currentSource &&
+                    <img
+                        data-src={currentSource} alt={alt}
+                        {...rest} className={`${styles.image} `}
+                        onLoad={e => e.target.setAttribute('style', 'opacity:1')}
+                    />}
+            </div>
+            }
             {/* this line is not working now, it expects to be in a SSR env to ensure that the images are indexable */}
-            <noscript>
+            {/* <noscript>
                 {!!currentSource && <img src={currentSource} alt={alt} />}
-            </noscript>
+            </noscript> */}
         </>
     )
 }
