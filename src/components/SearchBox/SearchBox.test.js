16e08a6e6dde3b981ab30a20e1a213bb2c8fd755
Danilo Vespa <danilo.vespa@everskill.de>
2022-01-18 18:55:45 +0100

    cover search box

--- src/components/SearchBox/SearchBox.test.js
+++ src/components/SearchBox/SearchBox.test.js
@@ -1,18 +1,52 @@
 import SearchBox from "./SearchBox";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-const mockedUsedNavigate = jest.fn();
+const mockedSetUserParams = {
+  append: jest.fn(),
+  get: jest.fn(),
+};
+
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockedUsedNavigate,
-  useSearchParams: () => [{ get: () => "test" }],
+  useSearchParams: () => [mockedSetUserParams, () => {}],
 }));
 
-test("render Message with children and duration", () => {
+test("render SearchBox", () => {
   let view = render(
     <>
       <SearchBox />
     </>
   );
   expect(view).toMatchSnapshot();
+});
+
+test("render SearchBox and emulate searching by clicking enter", () => {
+  render(
+    <>
+      <SearchBox />
+    </>
+  );
+  const input = screen.queryByPlaceholderText("Enter a search term");
+  expect(input.value).toBe("");
+  fireEvent.change(input, { target: { value: "Good Day" } });
+  expect(input.value).toBe("Good Day");
+  fireEvent.submit(input);
+  expect(mockedSetUserParams.append).toBeCalledTimes(1);
+  expect(mockedSetUserParams.append).toBeCalledTimes(1);
+});
+
+test("render SearchBox and emulate searching by clicking on button", () => {
+  render(
+    <>
+      <SearchBox />
+    </>
+  );
+  const input = screen.queryByPlaceholderText("Enter a search term");
+  expect(input.value).toBe("");
+  fireEvent.change(input, { target: { value: "Good Day" } });
+  expect(input.value).toBe("Good Day");
+  const button = screen.queryByText("Search");
+  fireEvent.click(button);
+  expect(mockedSetUserParams.append).toBeCalledTimes(1);
+  expect(mockedSetUserParams.append).toBeCalledTimes(1);
 });
